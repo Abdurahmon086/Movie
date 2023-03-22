04786a06ba4f3969bc03103e4a290fd3e6e6f50d
Abdurahmon086 <1937ali047@gmail.com>
2023-03-22 15:11:24 +0500

    third commit

--- js/main.js
+++ js/main.js
@@ -139,27 +139,22 @@ $('.search__form').addEventListener('submit', (e) => {
 // ============================= bookMark render UI start ============================
 
 function bookMarkUi(movies) {
+
     if (movies && movies.length) {
-        movies.forEach(item => {
+        movies.forEach((item, i) => {
             const card = createElement('div', 'card', `
-        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-        <img src="${item.minImg}" class="img-fluid w-100" />
-        <a href="#!"> <div class="mask"></div> </a>
-        </div>
-        <div class="card-body">
-        <h5 class="card-title">${item.name}</h5>
-        <div class="">
-        <ul class="list-group list-group-flush p-0">
-        <li class="list-group-item"> <strong>Year: </strong> ${item.year}  </li>
-        <li class="list-group-item"> <strong>Time: </strong> ${item.rtime}  </li>
-        <li class="list-group-item"> <strong>YouTube: </strong> <a href="${item.link}" target="_blank" >View </a>  </li>
-        <li class="list-group-item"><strong>Categories: </strong> ${item.category}</li>
-        </ul>
-        <div class="card-body p-0 d-flex justify-content-between align-items-center mt-3 gap-2">
-        <a href="#!" class="btn btn-primary">Read more</a>
-        </div>
-        </div>
-        `);
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+            <img src="${item.minImg}" class="img-fluid w-100" />
+            <a href="#!"> <div class="mask"></div> </a>
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <div >
+            <div class="card-body p-0">
+            <a href="#!" class="btn btn-danger " data-info="${i}">Delete</a>
+            </div>
+            </div>
+            `);
             $('.book-mark').append(card);
         })
     } else {
@@ -168,12 +163,15 @@ function bookMarkUi(movies) {
 
 };
 
+let data = JSON.parse(localStorage.getItem('bkArray'))
+bookMarkUi(data)
+
 // ============================= bookMark render UI and ============================
 
 // ============================= book mark btn start ============================
 
 $('.container-fluid').addEventListener("click", (e) => {
-    let bookMark = $(".book-mark")
+    let bookMark = $(".book__wrapper")
     if (e.target.matches('.btn-secondary')) {
         bookMark.classList.toggle('none')
     }
@@ -183,37 +181,66 @@ $('.container-fluid').addEventListener("click", (e) => {
 
 // ============================= get a card start ============================
 let bkArray = []
-let bkArray2 = []
 
 $('.wrapper').addEventListener('click', (e) => {
     $(".book-mark").innerHTML = ''
     if (e.target.matches('.bookmark__btn')) {
         let id = e.target.dataset.info
-        
-        let bookFind = movieAll.find(item => item.id === id)
-        bkArray.push(bookFind)
-        
-        bkArray.forEach(item => {            
-            let ok = true;
-            
-            for (let i = 0; i < bkArray2.length; i++) {
-                if (item.id === bkArray2[i].id) {
-                    ok = false
-                    break
-                }
-            }
-            
-            if(ok === true) {
-                bkArray2.push(item)
-            }
-        })
-        
-        if (bkArray2.length) {
-            console.log(bkArray2);
-            bookMarkUi(bkArray2)
+
+        let bookFind = movieAll.filter(item => item.id === id)[0];
+        if (!bkArray.includes(bookFind)) {
+            bkArray.push(bookFind)
+        }
+
+
+        // bkArray.forEach(item => {            
+        //     let ok = true;
+
+        //     for (let i = 0; i < bkArray2.length; i++) {
+        //         if (item.id === bkArray2[i].id) {
+        //             ok = false
+        //             break
+        //         }
+        //     }
+
+        //     if(ok === true) {
+        //         bkArray2.push(item)
+        //     }
+        // })1
+
+        if (bkArray.length) {
+            bookMarkUi(bkArray)
+
         } else {
             $(".book-mark").innerHTML = '<h1 class="text-center text-danger">hech nima yuq</h1>'
         }
+        let newObj = JSON.stringify(bkArray)
+        localStorage.setItem('bkArray', newObj)
     }
 })
 // ============================= get a card start ============================
+
+// ============================= delete start ============================
+
+$('.book__wrapper').addEventListener("click", (e) => {
+    $('.book-mark').innerHTML = ''
+    let bkArray = JSON.parse(localStorage.getItem('bkArray'))
+    if (e.target.classList.contains("btn-danger")) {
+        let id = e.target.getAttribute('data-info');
+        bkArray.splice(id, 1)
+
+        bookMarkUi(bkArray)
+
+        localStorage.setItem('bkArray', JSON.stringify(bkArray))
+    }
+})
+
+
+$('.book__wrapper').addEventListener('click', (e) => {
+    if (e.target.classList.contains("clear")) {
+        let bkClear = bkArray.length = 0
+
+        bookMarkUi(bkClear)
+        localStorage.clear()
+    }
+})
